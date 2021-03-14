d55851939cc4c2c247c9ed110c538fcc33f769f5
AustinJoo97 <austinjoo97@gmail.com>
2021-03-14 17:40:20 -0400

    added intializer function that will render all time stamps to the DOM upon loading web page

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,21 @@
-let timeEl = $('#currentDay');
+let currentDay = $('#currentDay');
+let timeStamps = $('#timeStamps');
+let dailyEvents = $('dailyEvents');
+let workDayHours = ['9AM', '10AM', '11AM', '12AM', '1PM', '2PM', '3PM', '4PM', '5PM']
 
-setInterval(function(){
-    timeEl.text(moment().format("dddd MMM Do, YYYY hh:mm:ss"))
-}, 1000)
+function initializer(){
+    currentDay.text(moment().format('dddd MMM Do, YYYY'));
+
+    for(let i = 0; i < workDayHours.length; i++){
+        if(i === workDayHours.length-1){
+            let nextTimeStamp = `<li id="hour${workDayHours[i]}" style="border-bottom: 2px solid cyan">${workDayHours[i]}</li>`;
+            timeStamps.append(nextTimeStamp);
+            break;
+        }
+        let nextTimeStamp = `<li id="hour${workDayHours[i]}">${workDayHours[i]}</li>`;
+        $('#timeStamps').append(nextTimeStamp);
+    }
+
+}
+
+initializer();
