bd000d7fab1ff01c7f0ad24e7b478f3c85752e1e
AustinJoo97 <austinjoo97@gmail.com>
2021-03-15 14:39:28 -0400

    added previously determined css classes to the custom html tags created in initializer function to add my own flavor into the program

--- assets/script.js
+++ assets/script.js
@@ -1,28 +1,28 @@
 let currentDay = $('#currentDay');
 let timeStamps = $('#timeStamps');
 let dailyEvents = $('#dailyEvents');
-let workDayHours = ['9AM', '10AM', '11AM', '12AM', '1PM', '2PM', '3PM', '4PM', '5PM']
+let saveButtons = $('#saveButtons')
+let workDayTimes = ['9AM', '10AM', '11AM', '12AM', '1PM', '2PM', '3PM', '4PM', '5PM']
+let workDayHours = [9, 10, 11, 12, 1, 2, 3, 4, 5];
 
 function initializer(){
     currentDay.text(moment().format('dddd MMM Do, YYYY'));
 
-    for(let i = 0; i < workDayHours.length; i++){
-        if(i === workDayHours.length-1){
-            timeStamps.append(`<li id="hour${workDayHours[i]}" style="border-bottom: 2px solid cyan">${workDayHours[i]}</li>`);
-            dailyEvents.append('<li style="border-bottom: 2px solid red"><input id="scheduleInput" ></input></li>');
+    for(let i = 0; i < workDayTimes.length; i++){
+        if(i === workDayTimes.length-1){
+            timeStamps.append(`<li class="time-block hour customTimeStamps" style="border-bottom: 2px solid cyan">${workDayTimes[i]}</li>`);
+
+            dailyEvents.append('<textarea class="customScheduleInput" style="border-bottom: 2px solid red"></textarea>');
+
+            saveButtons.append(`<button class="saveBtn customSaveBtn" style="border-bottom: 2px solid grey">Save</button>`)
             break;
         }
-        timeStamps.append(`<li id="hour${workDayHours[i]}">${workDayHours[i]}</li>`);
-        dailyEvents.append('<li><input id="scheduleInput"></input></li>');
-    }
+        timeStamps.append(`<li class="time-block hour customTimeStamps" id="hour${workDayTimes[i]}">${workDayTimes[i]}</li>`);
 
-    // for(let i = 0; i < workDayHours.length; i++){
-    //     if(i === workDayHours.length-1){
-    //         dailyEvents.append('<li style="border-bottom: 2px solid red"><input id="scheduleInput" ></input></li>');
-    //         break;
-    //     }
-    //     dailyEvents.append('<li><input id="scheduleInput"></input></li>');
-    // }
+        dailyEvents.append('<textarea class="customScheduleInput"></textarea>');
+
+        saveButtons.append(`<button class="saveBtn customSaveBtn">Save</button>`)
+    }
 }
 
 initializer();
