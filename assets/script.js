f2c412dda4ca9c1874e394b9a8bce3aff17e6279
AustinJoo97 <austinjoo97@gmail.com>
2021-03-15 13:22:43 -0400

    added initial second column list that will be rendered upon each load of the webpage; second column list will have input fields within each that will allow the user to enter daily event at corresponding timeslot

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,6 @@
 let currentDay = $('#currentDay');
 let timeStamps = $('#timeStamps');
-let dailyEvents = $('dailyEvents');
+let dailyEvents = $('#dailyEvents');
 let workDayHours = ['9AM', '10AM', '11AM', '12AM', '1PM', '2PM', '3PM', '4PM', '5PM']
 
 function initializer(){
@@ -8,14 +8,21 @@ function initializer(){
 
     for(let i = 0; i < workDayHours.length; i++){
         if(i === workDayHours.length-1){
-            let nextTimeStamp = `<li id="hour${workDayHours[i]}" style="border-bottom: 2px solid cyan">${workDayHours[i]}</li>`;
-            timeStamps.append(nextTimeStamp);
+            timeStamps.append(`<li id="hour${workDayHours[i]}" style="border-bottom: 2px solid cyan">${workDayHours[i]}</li>`);
+            dailyEvents.append('<li style="border-bottom: 2px solid red"><input id="scheduleInput" ></input></li>');
             break;
         }
-        let nextTimeStamp = `<li id="hour${workDayHours[i]}">${workDayHours[i]}</li>`;
-        $('#timeStamps').append(nextTimeStamp);
+        timeStamps.append(`<li id="hour${workDayHours[i]}">${workDayHours[i]}</li>`);
+        dailyEvents.append('<li><input id="scheduleInput"></input></li>');
     }
 
+    // for(let i = 0; i < workDayHours.length; i++){
+    //     if(i === workDayHours.length-1){
+    //         dailyEvents.append('<li style="border-bottom: 2px solid red"><input id="scheduleInput" ></input></li>');
+    //         break;
+    //     }
+    //     dailyEvents.append('<li><input id="scheduleInput"></input></li>');
+    // }
 }
 
 initializer();
